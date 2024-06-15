a10af0e29e111d70729bba1639943643dfd75719
C. Yunuen Olvera del C <cyunuenolveradelc@gmail.com>
2024-06-15 16:17:15 -0600

    Efectos JS cursor
    Creo que logré ponerle el efecto de cursor. La cosa es que a veces si se ve el '|' y a veces no. Intenté con otros plugins pero no lo logré mas que con https://safi.me.uk/typewriterjs/

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
 /* 
 Archivo de Java Script que contiene un script (conjunto de líneas de código que normalmente) 
+cambiar var(iable) a let
+todos los scripts van arriba del nuesto
+todos los css van arriba del nuestro
 */
-alert("HOLA")
+
+let app = document.getElementById('app');
+
+let typewriter = new Typewriter(app, {
+    loop: false,
+    deleteSpeed: 20,
+    cursor: '|',
+    cursorClassName: 'Typewriter__cursor',
+    wrapperClassName:'Typewriter__wrapper',
+    typeString: '|',
+    changeDelay:20,
+    string: true,
+    autoStart: false
+});
+
+typewriter
+.typeString('Me presento')
+    .pauseFor(500)
+    .deleteAll()
+    .typeString('Soy Yunuen Olvera')
+    .pauseFor(500)
+    .deleteChars(13)
+    .typeString('diseñadora gráfica jr')
+    .pauseFor(500)
+    .deleteChars(21)
+    .typeString('fotógrafa')
+    .pauseFor(500)
+    .deleteChars(9)
+    .typeString('#teamperros')
+    .pauseFor(500)
+    .start();
+   
